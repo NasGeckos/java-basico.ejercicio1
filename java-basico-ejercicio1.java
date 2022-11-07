bbede0198cd70350087b857ff313858a2b3428e8
NasGeckos <110292428+NasGeckos@users.noreply.github.com>
2022-11-07 18:51:08 +0100

    Create java-basico-ejercicio1.java

---
+++ java-basico-ejercicio1.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        // Tipo de dato e identificador = 30;
+        //tipo identificador; --- creo el identificador
+        //identificador = 30; --- le asigno un valor
+
+        // Entero
+        byte numer1 = 1; // 1 byte
+        short number2 = 2; // 2 byte
+        int number3 = 3; // 4 byte
+        long number4 = 4; // 8 byte
+
+        // Punto flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99; // Tiene mas capacidad que el float...
+
+        // Caracter
+        char caracter1 = 'M'; // Estan pensados solo para UN caracter.
+
+        // Boleanos
+        boolean verdadero = true;
+        boolean flaso = false;
+
+        // Cadenas de texto
+        String nombre = "Alan";
+        String apellido = "Sander";
+
+        // Tipos envoltorios: son para contener Null
+
+        Integer numero = null;
+        Long numero2 = 2L; // puede llevar una L mayusculas
+
+
+    }
+}
